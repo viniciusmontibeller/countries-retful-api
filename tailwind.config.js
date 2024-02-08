1897f7129ea299e3e055fb2abc24a4c4ec75ab92
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2024-02-07 21:36:51 -0300

    build: added base project structure and dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
+  darkMode: ['class'],
+  content: [
+    './index.html',
+    './src/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
     extend: {},
   },
