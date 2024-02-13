6f135cc6e15657d22dd488679384b43faf641b62
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2024-02-13 14:44:06 -0300

    feat: specified interfaces in one file

---
+++ src/interfaces.ts
@@ -0,0 +1,38 @@
+export interface NativeName {
+    [key: string]: {
+        common: string
+    }
+}
+
+export interface Currencies {
+    [key: string]: {
+        name: string
+    }
+}
+
+export interface Country {
+    name: {
+        common: string
+        nativeName?: NativeName
+    }
+    population: number
+    region: string
+    capital: string[]
+    flags: {
+        alt: string
+        svg: string
+    }
+}
+
+export interface CountryDetails extends Country {
+    subregion: string
+    tld: string[]
+    currencies: {
+        [key: string]: {
+            name: string
+        }
+    }
+    languages: {
+        [key:string]: string
+    }
+}
