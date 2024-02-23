f62be936a10c0f02ed4379fd756592482881a6fd
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2024-02-23 09:43:22 -0300

    feat: updated header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,16 @@
 import { Link } from "react-router-dom"
+import { ToggleTheme } from "./ToggleTheme"
 
 export const Header = () => {
     return (
-        <header className="flex shadow-lg justify-center">
-            <div className="flex justify-between max-w-screen-xl w-full m-5">
+        <header className="flex shadow-lg justify-center bg-dark-text-n-light-elements">
+            <div className="flex justify-between items-center max-w-screen-xl w-full mx-4 my-7">
                 <Link to='/'>
-                    <h1>
+                    <h1 className="font-extrabold sm:text-xl">
                         Where in the world?
                     </h1>
                 </Link>
-                <p>
-                    Dark Mode
-                </p>
+                <ToggleTheme />
             </div>
         </header>
     )
