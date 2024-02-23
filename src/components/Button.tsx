95b9ad72b79f88a34e91868d0cad2b56ee9eaa01
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2024-02-23 09:39:25 -0300

    feat: add button component

---
+++ src/components/Button.tsx
@@ -0,0 +1,27 @@
+import { ReactNode } from "react"
+import { Link } from "react-router-dom"
+import { cn } from "@/lib/utils"
+
+interface IButton extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    variant: 'default' | 'primary' | 'secondary'
+    children: ReactNode
+    link?: string
+}
+
+export const Button = ({ variant, link, children, className, ...props }: IButton) => {
+    return (
+        <>
+            {link ? (
+                <Link to={link} className="block w-fit">
+                    <button className={cn(`flex gap-2 items-center ${variant}`, className)} {...props}>
+                        {children}
+                    </button>
+                </Link>
+            ) : (
+                <button className={cn(`flex gap-2 items-center ${variant}`, className)} {...props}>
+                    {children}
+                </button>
+            )}
+        </>
+    )
+}
