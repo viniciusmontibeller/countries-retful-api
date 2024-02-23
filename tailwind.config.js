d5e62b735323990a2befde25fab1e465dec1b00d
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2024-02-23 09:33:21 -0300

    build: extended tailwind with clsx and merge. Added svgr for svgs

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,19 @@ export default {
     './src/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'dark-bg': 'hsl(var(--dark-mode-background))',
+        'light-bg': 'hsl(var(--light-mode-background))',
+        'light-text': 'hsl(var(--light-mode-text))',
+        'light-inputs': 'hsl(var(--light-mode-input))',
+        'dark-elements': 'hsl(var(--dark-mode-elements))',
+        'dark-text-n-light-elements': 'hsl(var(--dark-mode-text-n-light-mode-elements))'
+      },
+      fontFamily: {
+        nunito: ['Nunito-Sans', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
