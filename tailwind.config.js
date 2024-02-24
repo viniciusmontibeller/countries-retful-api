aff697d88a7e78f58b3c60ee8b1acacec302b7fe
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2024-02-24 18:33:09 -0300

    feat: add support config to accept themes changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,12 +8,10 @@ export default {
   theme: {
     extend: {
       colors: {
-        'dark-bg': 'hsl(var(--dark-mode-background))',
-        'light-bg': 'hsl(var(--light-mode-background))',
-        'light-text': 'hsl(var(--light-mode-text))',
-        'light-inputs': 'hsl(var(--light-mode-input))',
-        'dark-elements': 'hsl(var(--dark-mode-elements))',
-        'dark-text-n-light-elements': 'hsl(var(--dark-mode-text-n-light-mode-elements))'
+        background: 'hsl(var(--background-color), 1)',
+        text: 'hsl(var(--text-color), 1)',
+        element: 'hsl(var(--element-color), 1)',
+        input: 'hsl(var(--input-color), 1)',
       },
       fontFamily: {
         nunito: ['Nunito-Sans', 'sans-serif']
