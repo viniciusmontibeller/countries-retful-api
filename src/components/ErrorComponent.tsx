172635c169fef4f855fb183a190b496eb73f5b95
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2024-03-18 18:57:12 -0300

    feat: add refresh option and styled the error component

--- src/components/ErrorComponent.tsx
+++ src/components/ErrorComponent.tsx
@@ -1,12 +1,24 @@
+import { useNavigate } from "react-router-dom"
+import { Button } from "./Button"
+
 interface ErrorComponentProps {
     error: string
 }
 
 export const ErrorComponent = ({ error }: ErrorComponentProps) => {
+    const navigate = useNavigate()
+
+    const handleRefresh = () => {
+        navigate(0)
+    }
+
     return (
-        <div>
-            <h1>Error ❌</h1>
-            <p>{error}</p>
+        <div className="space-y-6">
+            <div className="text-center space-y-6 bg-element rounded-md p-4 shadow-lg">
+                <p className="text-xl">{error}</p>
+                <p className="text-sm text-faded">Try reloading the page or come back later</p>
+            </div>
+            <Button className="mx-auto" variant="primary" onClick={handleRefresh} >Refresh</Button>
         </div>
     )
 }
