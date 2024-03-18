0a083e91247b26b78bdc116485b150d55811cad5
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2024-03-18 17:53:17 -0300

    feat: created a not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,10 +2,13 @@ import { Link } from "react-router-dom"
 
 export const NotFound = () => {
     return (
-        <div>
-            <h2>Page not found!</h2>
+        <div className="text-faded space-y-7">
+            <span className="tracking-widest text-8xl">404</span>
+            <h2 className="text-center">Page not found!</h2>
 
-            <p>Go to <Link to={'/'}>Homepage</Link>.</p>
+            <p className="text-center">Go to back{' '}
+                <Link className="text-text text-xl hover:border-b hover:border-input" to={'/'}>Homepage</Link>
+            </p>
         </div>
     )
 }
