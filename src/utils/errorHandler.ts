37e7e81e6a2889f8feb14ec25b833a46e329d703
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2024-03-18 17:52:56 -0300

    feat: added error tipe

---
+++ src/utils/errorHandler.ts
@@ -0,0 +1,29 @@
+import axios, { AxiosError } from "axios"
+
+const getErrorMessage = (error: unknown): string => {
+    if (error instanceof Error) {
+        return error.message
+    } else if (error && typeof error === 'object' && 'message' in error) {
+        return String(error.message)
+    } else if (typeof error === 'string') {
+        return error
+    }
+    
+    return 'Something went wrong'
+}
+
+const handleAxiosError = (error: AxiosError): string => {
+    if (error.response) {
+        return `HTTP Error: ${error.response.status}`
+    } else {
+        return `Network Error: ${error.message}`
+    }
+}
+
+export const errorHandler = (error: unknown) => {
+    if (axios.isAxiosError(error)) {
+        return handleAxiosError(error as AxiosError)
+    } else {
+        return `Error: ${getErrorMessage(error)}`
+    }
+}
