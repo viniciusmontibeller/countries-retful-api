1665459d7dd0f2d5df69ec5c3c54ca07dc57eccc
Vinicius da Silva Montibeller <vinism96@hotmail.com>
2024-04-17 20:01:11 -0300

    refacotr: tried to fix infite fetch problem

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,61 +1,38 @@
-import { useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import { errorHandler } from "@/utils/errorHandler"
-// import { useOnLoad } from "./useOnLoad"
 import { client } from "./client"
-import axios, { AxiosRequestConfig, AxiosResponse } from "axios"
-import { useOnLoad } from "./useOnLoad"
+import axios, { AxiosRequestConfig, AxiosResponse,  } from "axios"
 
 export const useFetch = <T,>(url: string, config?: AxiosRequestConfig, shouldFetch = true) => {
     const [data, setData] = useState<T | null>(null)
     const [loading, setLoading] = useState(true)
     const [error, setError] = useState<string | null>(null)
 
-    useOnLoad(() => {
-        const fetchData = async () => {
-            const controller = new AbortController()
-                try {
-                    const response: AxiosResponse<T> = await client.get(url, {
-                        ...config,
-                        signal: controller.signal
-                    })
-                    const newData = response.data
-                    setData(newData)
-                    setError(null)
-                } catch (error) {
-                    if(axios.isCancel(error)) console.log(error)
-                    const errorInfo = errorHandler(error)
-                    setError(errorInfo)
-                } finally {
-                    setLoading(false)
-                }
-                controller.abort()
+    const fetchData = useCallback(async () => {
+        const controller = new AbortController()
+        try {
+            const response: AxiosResponse<T> = await client.get(url, {
+                ...config,
+                signal: controller.signal
+            })
+            const newData = response.data
+            setData(newData)
+            setError(null)
+        } catch (error) {
+            if(axios.isCancel(error)) return
+            const errorInfo = errorHandler(error)
+            setError(errorInfo)
+        } finally {
+            setLoading(false)
         }
+        controller.abort()
+    }, [url, config])
+
+    useEffect(() => {
         if (shouldFetch) {
             fetchData()
         }
-        // return () => controller.abort()
-    }, [url, config, shouldFetch])
-
-    // async function fetchData() {
-    //     const controller = new AbortController()
-
-    //     try {
-    //         const response: AxiosResponse<T> = await client.get(url, {
-    //             ...config,
-    //             signal: controller.signal
-    //         })
-    //         const data = response.data
-    //         setData(data)
-    //         setError(null)
-    //     } catch (error) {
-    //         const errorInfo = errorHandler(error)
-    //         setError(errorInfo)
-    //     } finally {
-    //         setLoading(false)
-    //     }
-
-    //     controller.abort()
-    // }
+    }, [shouldFetch, fetchData])
 
     return { data, loading, error }
 }
